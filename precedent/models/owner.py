8b1e2de52e7bc513b2e59f3fe2a588f4b5cd402e
Jared Lewis <jared@jared.kiwi.nz>
2017-12-18 23:22:04 +1300

    Added source field to Owner model

--- precedent/models/owner.py
+++ precedent/models/owner.py
@@ -8,8 +8,15 @@ class Owner(models.Model):
         user = ChoiceItem('U')
         organisation = ChoiceItem('O')
 
+    class Source(DjangoChoices):
+        github = ChoiceItem('gh')
+
     remote_id = models.IntegerField()
     name = models.TextField()
     type = models.CharField(max_length=1, choices=OwnerType.choices)
     url = models.URLField()
     avatar_url = models.URLField(null=True)
+    source = models.CharField(max_length=2, choices=Source.choices)
+
+    def __str__(self):
+        return '<Owner name={} type={}>'.format(self.name, self.type)
