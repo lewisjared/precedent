5cace0b033977132ea3ba5d5c1556244634f09a1
Jared Lewis <jared@jared.kiwi.nz>
2018-01-07 10:51:30 +1300

    Productionified the settings

--- precedent/settings.py
+++ precedent/settings.py
@@ -10,16 +10,9 @@ load_dotenv(find_dotenv())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6#%chg&%9op!tn5g((1beh@upe=5a$y+_cu@vzth6#s$0$vb$*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', '6#%chg&%9op!tn5g((1beh@upe=5a$y+_cu@vzth6#s$0$vb$*')
+DEBUG = os.environ.get('DEBUG', 'true') != 'false'
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -93,8 +86,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.environ.get('STATIC_ROOT')
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT')
